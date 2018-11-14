a60abb6328505ddc13bf1983f7d06f2589940655
Philippe MOUIEL <philippemouiel@mbp-de-philippe.lan>
2018-11-14 13:00:04 +0100

    update 13h

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from "react";
 import "./App.css";
 import Header from "./Header";
-
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+const axios = require("axios");
 class App extends Component {
   render() {
     return (
       <div id="container">
         <Header />
+        <Router>
+          <div>
+            <Link to="/">Home</Link>
+            <hr />
+            <Link to="/about">About</Link>
+            <hr />
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+          </div>
+        </Router>
       </div>
     );
   }
